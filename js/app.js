ea64db607d2a7366a3edec906c70419156ef0c33
Sergio <sergio.rsal@gmail.com>
2025-06-10 21:49:53 -0600

    Se crean las funciones de ingresos y egresos así como las clases

---
+++ js/app.js
@@ -0,0 +1,51 @@
+
+const ingresos = [
+  new Ingreso('Salario', 20000),
+  new Ingreso('Venta auto', 50000)
+];
+
+const egresos = [
+  new Egreso('Renta', 4000),
+  new Egreso('Ropa', 800)
+];
+
+const totalIngresos = () => {
+    let total = 0;
+
+    for (let ingreso of ingresos) {
+        total += ingreso.valor;
+    }
+
+    return total;
+};
+
+const totalEgresos = () => {
+    let total = 0;
+
+    for (let egreso of egresos) {
+        total += egreso.valor;
+    }
+
+    return total;
+};
+
+
+const formatoMoneda = (valor) => {
+    return valor.toLocaleString('es-MX', {
+        style: 'currency', 
+        currency: 'MXN',
+        minimumFractionDigits: 2
+    });
+};
+
+const formatoPorcentaje = (valor) => {
+    return valor.toLocaleString('es-MX', {
+        style: 'percent',
+        minimumFractionDigits: 2
+    });
+};
+
+const cargarCabecero = () => {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let procentajeEgreso = totalEgresos()/totalIngresos();
+}   
