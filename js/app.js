cbe8e9e1be5975358775054bbfe5563f06c74119
Sergio <sergio.rsal@gmail.com>
2025-06-11 20:48:47 -0600

    Se agregaron varias funciones a apps

--- js/app.js
+++ js/app.js
@@ -48,4 +48,68 @@ const formatoPorcentaje = (valor) => {
 const cargarCabecero = () => {
     let presupuesto = totalIngresos() - totalEgresos();
     let porcentajeEgreso = totalEgresos()/totalIngresos();
-}   
+
+    document.getElementById("presupuesto").innerHTML = formatoMoneda(presupuesto);
+    document.getElementById("porcentaje").innerHTML = formatoPorcentaje(porcentajeEgreso);
+    document.getElementById("ingresos").innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById("egresos").innerHTML = formatoMoneda(totalEgresos());
+
+};    
+
+const cargarIngresos = () => {
+    let ingresosHTML = "";
+
+    for (let ingreso of ingresos) {
+        ingresosHTML += crearIngresoHTML(ingreso)
+    }
+    document.getElementById("lista-ingresos").innerHTML = ingresosHTML;
+};
+
+const crearIngresoHTML = (ingreso) => {
+    return `
+        <div class="elemento limpiarEstilos">
+            <div class="elemento_descripcion">${ingreso.desciprcion}</div>
+            <div class="derecha limpiarEstilos">
+                <div class="elemento_valor>+ ${formatoMoneda(ingreso.valor)}</div>
+                <div class="elemento_eliminar">
+                    <button class="elemento_eliminar--btn" onclick="eliminarIngreso(${ingreso.id})">
+                        <ion-icon name="close-circle-outline"></ion-icon>
+                    </button>
+                </div>
+            </div>
+        </div>
+    `;
+};
+
+const cargarEgresos = () => {
+    let egresosHTML = "";
+
+    for (let egreso of egresos) {
+        egresosHTML += crearEgresoHTML(egreso);
+    }
+    document.getElementById("lista-egresos").innerHTML = egresosHTML;
+};
+
+const crearEgresoHTML = (egreso) => {
+    let porcentaje = egreso.valor / totalEgresos();
+
+    return `
+        <div class="elemento limpiarEstilos">
+            <div class="elemento_descripcion">${egreso.descripcion}</div>
+            <div class="derecha limpiarEstilos">
+                <div class="elemento_valor">- ${formatoMoneda(egreso.valor)}</div>
+                <div class="elemento_porcentaje">${formatoPorcentaje(porcentaje)}</div>
+                <div class="elemento_eliminar">
+                    <button class="elemento_eliminar--btn" onclick="eliminarEgreso(${egreso.id})">
+                        <ion-icon name="close-circle-outline"></ion-icon>
+                    </button>
+                </div>
+            </div>
+        </div>
+    `;
+};
+
+
+const cargarApp = () => {
+
+}
